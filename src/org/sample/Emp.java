0e591b387c92729b9ca093a82be1bd68d383ef70
prasanthraj1 <tprasanthraj.kv@gmail.com>
2022-05-09 09:41:00 +0530

    New Changes

--- src/org/sample/Emp.java
+++ src/org/sample/Emp.java
@@ -3,6 +3,8 @@ package org.sample;
 public class Emp {
 
 	public static void main(String[] args) {
-		System.out.println("First Change");
+		System.out.println("First Change"); 
+		System.out.println("Emp1 new changes");
 	}
+	
 }
